f91ba3f6b6486d58dd44659b8d682c7dba61ada4
MOHAMED IMRAN <110348277+Imjaz@users.noreply.github.com>
2022-09-09 10:22:45 +0530

    Add files via upload

---
+++ tables.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class HelloWorld {
+    public static void main(String[] args) {
+Scanner d=new Scanner(System.in);
+int s=d.nextInt();
+int t=d.nextInt();
+int temp=s;
+ s=t;
+t=temp;
+
+}
+}
