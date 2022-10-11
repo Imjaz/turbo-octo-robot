1515628c9427366796fa3fd30907116ab62cc074
MOHAMED IMRAN <110348277+Imjaz@users.noreply.github.com>
2022-10-11 21:07:04 +0530

    Add files via upload

--- train.java
+++ train.java
@@ -1,4 +1,4 @@
-import java.util.*;
+ import java.util.*;
 class train
 {
 	public static void main(String[]args)
@@ -20,7 +20,8 @@ class train
 			Bt.BookTickets();
 			break;
 			case 3:
-			
+			cancel as=new cancel();
+                                                as.cancel();
 			break;
 			case 4:
 			BookedTicket Bdt=new BookedTicket();
@@ -41,14 +42,14 @@ class passengerDetails
 	 static int totalseat=6;
 	 static int Rac=2;
 	 static  int wl=2;
-		static String Pnr[]=new String[10];
-		static String Name[]=new String[10];
-		static int Age[]=new int[10];
-	    static int Seatno[]=new int[10];
-	    static String Gender[]=new String[10];
-		static int Fare[]=new int[10];
-		static String Berth[]=new String[10];
-	
+		static String Pnr[]=new String[12];
+		static String Name[]=new String[12];
+		static int Age[]=new int[12];
+	    static int Seatno[]=new int[12];
+	    static String Gender[]=new String[12];
+		static int Fare[]=new int[12];
+		static String Berth[]=new String[12];
+static int serial[]=new int[12];
 	
 }
 class BookTicket extends passengerDetails
@@ -57,11 +58,12 @@ class BookTicket extends passengerDetails
 	 static String pnr;
 	 static int count=0;
 	 static int seno=1;
-static int n=0;
+
+static int  sino=0;
 	static BookTicket obj=new BookTicket();
 	void BookTickets()
-	{	System.out.println("Enter how many ticket you want to book");
-		 n=scan.nextInt();
+	{	System.out.println("how many ticket you want to book'it should be <=10");
+		int n=scan.nextInt();
 if(n<=10){
 		if(seno<=6)
 		{
@@ -70,17 +72,17 @@ if(n<=10){
 		}
 		else if(seno>6 && seno<=8)
 		{
-			pnr="RAC";
-Berth[seno-1]="RAC";
-
+			pnr=Integer.toString(pvalue);
+                                              
 			obj.setTicket(n,pnr);
 			
 
 		}
 		else if(seno>8 && seno<=10)
 		{
-			pnr="WL";
-Berth[seno-1]="Wl";
+
+			pnr=Integer.toString(pvalue);
+                                               Berth[n]="Wl";
 
 			obj.setTicket(n,pnr);
 		}
@@ -91,7 +93,7 @@ obj.BookTickets();}
 	void setTicket(int n,String pnr)
 	{
 			for(int i=0;i<n;i++)
-		{
+		{serial[count]=++sino;
 			Pnr[count]=pnr;
 			System.out.println("Enter the Passenger Name");
 			String na=scan.next();
@@ -102,10 +104,17 @@ obj.BookTickets();}
 			if(ag>5)
 			Seatno[count]=seno++;
 		   else 
-			   Seatno[count]=0;
+			   Seatno[count]=seno;
 			System.out.println("Enter the Passenger Gender");
 			String gen=scan.next();
 			Gender[count]=gen;
+if(lower==0&&middle==0&&upper==0&&Rac==0){
+Berth[count]="WL";
+--wl;}
+else if(lower==0&&middle==0&&upper==0){
+Berth[count]="RAC";
+--Rac;
+}
 			if(ag>5)
 				Fare[count]=400;
 			else
@@ -199,12 +208,12 @@ class BookedTicket extends passengerDetails
 {
 	void showTicket()
 	{
-		System.out.print("Pnr\tName\tAge\tSeatno\tGender\tFare\tBerth\n");
+		System.out.print("serial\tPnr\tName\tAge\tSeatno\tGender\tFare\tBerth\n");
 		for(int i=0;i<Name.length;i++)
 		{	
 		if(Fare[i]!=0)
 		{
-		System.out.print(Pnr[i]+"\t"+Name[i]+"\t"+Age[i]+"\t"+Seatno[i]+"\t"+Gender[i]+"\t"+Fare[i]+"\t"+Berth[i]+"\n");
+		System.out.print(serial[i]+"\t"+Pnr[i]+"\t"+Name[i]+"\t"+Age[i]+"\t"+Seatno[i]+"\t"+Gender[i]+"\t"+Fare[i]+"\t"+Berth[i]+"\n");
         }
 		}
 	}
@@ -217,4 +226,43 @@ class AvailableTicket extends passengerDetails
 	 System.out.println("No.of.Lower No.of.Upper No.of.Middle Total seats");
 	 System.out.println("    "+lower+"\t        "+upper+"\t    "+middle+"\t"+totalseat);
 	}
+}
+class cancel extends passengerDetails{
+
+void cancel(){
+System.out.println("enter the serial no.:");
+int v=scan.nextInt();
+int i =0;
+if(serial[v-1]==v){
+Pnr[v-1]=null;
+Age[v-1]=0;
+Fare[v-1]=0;
+Gender[v-1]=null;
+Name[v-1]=null;
+for( i=0;i<serial.length;i++){
+if(Berth[i].equals("RAC")){
+break;
+}
+}
+String temp=Name[i];
+Name[i]=Name[v-1];
+Name[v-1]=temp;
+String temp7=Name[i];
+Name[i]=Name[i+1];
+Name[i+1]=temp7;
+
+int temp1=Age[i];
+Age[i]=Age[v-1];
+Age[v-1]=temp1;
+
+String temp2=Gender[i];
+Gender[i]=Gender[v-1];
+Gender[v-1]=temp2;
+
+String temp3=Pnr[i];
+Pnr[i]=Pnr[v-1];
+Pnr[v-1]=temp3;
+}
+
+}
 }
